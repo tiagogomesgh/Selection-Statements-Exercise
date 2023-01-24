c9c4bfedd9a93966d2e5ee741cf74088c4bb4f11
tiagogomesgh <tiagogomesfl@outlook.com>
2023-01-23 18:20:28 -0800

    Finished SelectionStatements1 Exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Threading.Channels;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Can you guess my favorite number???");
+            FavoriteNumber();
         }
+
+        static bool FavoriteNumber()
+        {
+            
+
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            
+                
+            if (userInput == favNumber)
+                {
+                    Console.WriteLine("You Got it");
+                    return true;
+                }
+                else
+                {
+                    
+                    return false;
+                }
+            
+        }
+        //var r = new Random();
+        //var favNumber = r.Next(1,1000);
+        //var userInput = int.Parse(Console.ReadLine());
     }
 }
